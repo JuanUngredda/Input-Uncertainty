0b18c53f7e558448f5c74847123523b62a3cdd44
JuanUngredda <j.ungredda@warwick.ac.uk>
2021-01-10 16:18:38 +0000

    Production Line fp

--- ModularVersion/runner_Production_Line_fp.py
+++ ModularVersion/runner_Production_Line_fp.py
@@ -46,7 +46,7 @@ def function_caller(rep):
 	Information_Source_Generator = Information_Source(Distribution=True_Input_distributions, lb=Simulator.amin,
 													  ub=Simulator.amax, d=1)
 
-	proportions = np.linspace(5,40,15)
+	proportions = np.linspace(5,40,10)
 	for i in proportions:
 		[XA], [Y], [Data] = myoptimizer( sim_fun = Simulator, inf_src= Information_Source_Generator,
 						  lb_x = Simulator.xmin, ub_x = Simulator.xmax,
